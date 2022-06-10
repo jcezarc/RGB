a9a98c938ff0f1b70497502765b04979dc7ea0d8
Júlio Cascalles <jcezarc@yahoo.com.br>
2022-06-10 14:36:12 -0300

    Adicionando instruções

--- __main__.py
+++ __main__.py
@@ -1,7 +1,20 @@
 from jogo import Jogo
 
+print('''
+    ::::::  R.G.B. ::::::
+    Separe as cores nos tubos
+    até não ter cores misturadas.
 
+    Como jogar: Digite o número do tubo de origem
+    seguido do número do tubo de destino.
+    Exemplo: "1 3" ou "1-3" ou "13"
+    significa que o tubo 1 deve ser despejado no tubo 3
+
+    Bom jogo!
+''')
 jogo = Jogo(5)  # 5 tubos de 5 cores cada
 while jogo.continuar():
     jogo.atualiza()
-print('==== PARABÉNS!!! ====')
+print('=' * 50)
+print('PARABÉNS'.center(50))
+print('=' * 50)
